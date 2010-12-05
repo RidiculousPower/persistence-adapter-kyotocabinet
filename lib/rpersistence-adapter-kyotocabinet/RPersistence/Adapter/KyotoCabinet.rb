5bf0b9415bb36c98ba85b9375c3972cb1d8ddc1b
Asher <asher@ridiculouspower.com>
2010-12-05 06:56:17 -0500

    Initial commit

---
+++ lib/rpersistence-adapter-kyotocabinet/RPersistence/Adapter/KyotoCabinet.rb
@@ -0,0 +1,11 @@
+# Rpersistence::Adapter::KyotoCabinet
+#
+# Rpersistence Adapter for Kyoto Cabinet
+#
+# => Not yet written. 
+# => If you are familiar with Kyoto Cabinet, please fork, write, and request a pull.
+
+class Rpersistence::Adapter::KyotoCabinet
+  
+end
+
---
+++ lib/rpersistence-adapter-kyotocabinet/rpersistence-adapter-kyotocabinet.rb
@@ -0,0 +1,8 @@
+require_relative 'RPersistence/Adapter/KyotoCabinet.rb'
+
+class RPersistence
+  class Adapter
+    class KyotoCabinet
+    end
+  end
+end
